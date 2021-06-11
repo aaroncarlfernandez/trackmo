c3aab6af9b80eee00a0b022d4bc3bb54beb7db45
Aaron Carl Fernandez <actfernandez@mymail.mapua.edu.ph>
2021-06-11 20:40:43 +0800

    Created out register page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+
+import AppNavBar from '../components/NavBar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <AppNavBar />
+      <Container> 
+        <Component {...pageProps} />
+      </Container>
+    </>
+  )
 }
 
 export default MyApp
